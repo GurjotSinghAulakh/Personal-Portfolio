66e3eeda69383ab0c9eedef3963539ce8ad2deae
Gurjot Singh Aulakh <s351873@oslomet.no>
2022-06-12 23:30:36 +0200

    Personal Portfolio - V6.0
    - Created Testimonials Component JSX
    
    - Designed Testimonials Component using CSS (RESPONSIVE)
    
    - Generated Testimonials Items from Array using map method
    
    - Added some testimonials from my colleagues

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -22,6 +22,6 @@ const Header = () => {
         </div>
     </header>
   )
-}
+} 
 
 export default Header
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avtar1.jpeg'
+import AVTR2 from '../../assets/avtar2.jpg'
+import AVTR3 from '../../assets/avtar3.jpeg'
+import AVTR4 from '../../assets/avtar4.jpeg'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Mortaza',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aut dolore provident voluptatibus eum iste atque eius. Libero ipsam similique, sit tempora quaerat cum hic laudantium error repellat ea eligendi',
+  }, 
+  {
+    avatar: AVTR2,
+    name: 'Elias',
+    review: 'Based on my observations of you, I can conclude that you work exceptionally hard and love working in challenging conditions. You are responsible, dependable, and like working in a group setting. You are an extremely helpful person who spends time assisting others. Furthermore, I perceive that you have a natural ability to lead a group.',
+  }, 
+  {
+    avatar: AVTR3,
+    name: 'Tharusan',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aut dolore provident voluptatibus eum iste atque eius. Libero ipsam similique, sit tempora quaerat cum hic laudantium error repellat ea eligendi',
+  }, 
+  {
+    avatar: AVTR4,
+    name: 'Arman ',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aut dolore provident voluptatibus eum iste atque eius. Libero ipsam similique, sit tempora quaerat cum hic laudantium error repellat ea eligendi',
+  } 
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+      // Install Swiper Modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+      {
+        data.map(({avatar, name, review}, index) => {
+          return (
+            <SwiperSlide key={index} className='testimonial'>
+            <div className="client__avatar">
+              <img src={avatar}/>
+            </div>
+            <h5 className='client_name'>{name}</h5>
+            <small className='client__review'>{review}</small>
+          </SwiperSlide>
+          )
+        })
+      }
+
+      </Swiper>
+    </section>
   )
 }
 
