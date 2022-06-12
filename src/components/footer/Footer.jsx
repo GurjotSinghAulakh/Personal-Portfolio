669f0078868398368d29a56c87138b3beb8611ce
Gurjot Singh Aulakh <s351873@oslomet.no>
2022-06-13 01:48:10 +0200

    Personal Portfolio - V8.0
    - Creates Footer Component JSX
    
    - Designed Footer Component using CSS (RESPONSIVE)
    
    - Deploying my React Website/ Portfolio on github pages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>GURJOT SINGH AULAKH</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/gurjotsingh.aulakh.1/"><FaFacebookF/></a>
+        <a href="https://www.instagram.com/stylish_singh01/"><FiInstagram/></a>
+        <a href="https://www.linkedin.com/in/gurjotsinghaulakh/"><BsLinkedin/></a>
+        <a href="https://github.com/GurjotSinghAulakh"><BsGithub/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Future Solutions As. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
