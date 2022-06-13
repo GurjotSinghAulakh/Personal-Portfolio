487d30ba9e51895f830bfc1cb448fb0ea84f7744
Gurjot Singh Aulakh <s351873@oslomet.no>
2022-06-13 02:14:10 +0200

    Updated Testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -19,12 +19,12 @@ const data = [
   {
     avatar: AVTR1,
     name: 'Mortaza',
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aut dolore provident voluptatibus eum iste atque eius. Libero ipsam similique, sit tempora quaerat cum hic laudantium error repellat ea eligendi',
+    review: "We became friends during the introductory week, where we worked together on the innovation camp assignment. I quickly realized that he was a kind and hard working guy with a lot of motivation. After this, me and Gurjot have become good friends and worked together with all compulsory tasks together in school. In addition, we have worked together in many of the jobs we have had in common together such as teaching assistant in programming, student ambassador for TKD oslomet and Secundo AS. Gurjot is a generous man, who puts the needs of others before his own. He works well in teams, communicates well, does the work he undertakes and produces good results. He does his best to help and uplift others, and does not hesitate to share experiences and knowledge he has with others :) I'm glad I got to know you, and look forward to working on many more projects together.",
   }, 
   {
     avatar: AVTR2,
     name: 'Elias',
-    review: 'Based on my observations of you, I can conclude that you work exceptionally hard and love working in challenging conditions. You are responsible, dependable, and like working in a group setting. You are an extremely helpful person who spends time assisting others. Furthermore, I perceive that you have a natural ability to lead a group.',
+    review: 'Based on my observations, I can conclude that Gurjot work exceptionally hard and love working in challenging conditions. He are responsible, dependable, and like working in a group setting. Gurjot are an extremely helpful person who spends time assisting others. Furthermore, I perceive that he have a natural ability to lead a group.',
   }, 
   {
     avatar: AVTR3,
