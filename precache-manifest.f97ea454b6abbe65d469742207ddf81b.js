5deb8161164bf8d4f32b3cd68135bccf4acc98e5
GurjotSinghAulakh <s351873@oslomet.no>
2022-08-16 15:58:58 +0200

    Updates

---
+++ precache-manifest.f97ea454b6abbe65d469742207ddf81b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "04236d93b300ae41774a",
+    "url": "/Personal-Portfolio/static/css/main.6bffabe6.chunk.css"
+  },
+  {
+    "revision": "04236d93b300ae41774a",
+    "url": "/Personal-Portfolio/static/js/main.04236d93.chunk.js"
+  },
+  {
+    "revision": "224b9344731e2137ac3f",
+    "url": "/Personal-Portfolio/static/css/1.d701da6f.chunk.css"
+  },
+  {
+    "revision": "224b9344731e2137ac3f",
+    "url": "/Personal-Portfolio/static/js/1.224b9344.chunk.js"
+  },
+  {
+    "revision": "e59b4bbb724cec86d1ab",
+    "url": "/Personal-Portfolio/static/js/runtime~main.e59b4bbb.js"
+  },
+  {
+    "revision": "04856633fe97b83e1733293f4b840dfc",
+    "url": "/Personal-Portfolio/static/media/cv.04856633.pdf"
+  },
+  {
+    "revision": "33499492e71d58ef113182641c39b3a8",
+    "url": "/Personal-Portfolio/static/media/Singh-img.33499492.png"
+  },
+  {
+    "revision": "a933775f403b9c30632e04e6ffdcc621",
+    "url": "/Personal-Portfolio/static/media/img2.a933775f.jpg"
+  },
+  {
+    "revision": "25f5cd556ce6e52ab5d5d0f530515d79",
+    "url": "/Personal-Portfolio/static/media/portfolio-image1.25f5cd55.jpg"
+  },
+  {
+    "revision": "63f546c2832b5a958c5bb09b6cfff17c",
+    "url": "/Personal-Portfolio/static/media/portfolio-image2.63f546c2.jpg"
+  },
+  {
+    "revision": "4ced1610381a455f6d7c8c7145a3157e",
+    "url": "/Personal-Portfolio/static/media/portfolio-image3.4ced1610.jpg"
+  },
+  {
+    "revision": "63e9082c5d4347c5487c3dd0d3027b63",
+    "url": "/Personal-Portfolio/static/media/portfolio-image4.63e9082c.jpg"
+  },
+  {
+    "revision": "b5cca163d206dada536dc58bdecd5bb4",
+    "url": "/Personal-Portfolio/static/media/portfolio-image5.b5cca163.jpg"
+  },
+  {
+    "revision": "ef2d28a21b4b7889075f3778d45b34f1",
+    "url": "/Personal-Portfolio/static/media/avtar1.ef2d28a2.jpeg"
+  },
+  {
+    "revision": "482c7114366d636b8017b858dfc85138",
+    "url": "/Personal-Portfolio/static/media/avtar2.482c7114.jpg"
+  },
+  {
+    "revision": "5758a7aa0c991cc222db1dc7b07fa194",
+    "url": "/Personal-Portfolio/static/media/avtar3.5758a7aa.jpeg"
+  },
+  {
+    "revision": "9234160206c7599b269abf16354b4112",
+    "url": "/Personal-Portfolio/index.html"
+  }
+];
